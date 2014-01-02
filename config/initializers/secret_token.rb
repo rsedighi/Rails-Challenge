5eafbf907c154a8cbaf5e2bfe26f77f889b48713
Ramin <rsedighi@gmail.com>
2014-01-01 19:51:51 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = 'a0d989033e7228d6d648a7b3eab675bd02a2f13dbc35925522321350361281c5e8ff050a10616831caf056f3eed5bc67e63e0dfa83b9f80bc07c92e32432a36a'
